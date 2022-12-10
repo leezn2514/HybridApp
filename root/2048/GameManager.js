9f3c64e6b70cebed39f2e5b80ac394e21a85e17c
아현 이 <ryunyu2514@gmail.com>
2022-12-10 09:34:33 +0900

    [2022-12-10] Add to Final Exam 3

--- root/2048/GameManager.js
+++ root/2048/GameManager.js
@@ -1,10 +1,11 @@
 // Memory pool을 이용해 재활용 가능한 block 객체를 구현하기.
-
+var i, j;
 var empty_arr = new Array(16); // 비어있는 공간 확인용 배열
-for(var i = 0; i < empty_arr.length; i++) empty_arr[i] = 0;
+for(i = 0; i < empty_arr.length; i++) empty_arr[i] = 0;
 
 var addBlockPlace = document.getElementById('background');
 var bg = document.getElementsByClassName('bg_block');
+var blocks = document.getElementsByClassName("fillBox");
 
 const {createApp} = Vue;
   createApp({
@@ -18,84 +19,105 @@ const {createApp} = Vue;
     },
   }).mount("#background")
 
-class Block {
-  constructor(a) {
-    this.position_top_arr = a;
-    this.position_left_arr = a;
+class GameManager {
+  constructor() {
+    this.blockArr = [16];
+    this.Block = [16]
+    
+    this.blockArr_num = -1;
   }
 
-  RandomCreate() {
+  ObjectPool() {      
+    this.Block[this.blockArr_num] = new Block([24, 178, 332, 486], [24, 178, 332, 486]); // Block 객체 생성
+    this.blockArr.push(this.Block[this.blockArr_num]); 
+    this.blockArr_num++;
+
+    return this.blockArr;
+  }
+}
+
+
+class Block {
+  constructor(posX, posY) {
+    this.posX = posX;
+    this.posY = posY; 
+
     this.newBlock = document.createElement('div');
     this.newBlock.setAttribute('class', 'fillBox');
     addBlockPlace.appendChild(this.newBlock);
 
     var random_top_num = Math.floor(Math.random() * 4);
     var random_left_num = Math.floor(Math.random() * 4); 
-    var block_position = 4 * random_top_num + random_left_num;
 
+    var block_position = 4 * random_top_num + random_left_num;
     empty_arr[block_position] = 1;
-    this.newBlock.style.top = this.position_top_arr[random_top_num] + "px";
-    this.newBlock.style.left = this.position_left_arr[random_left_num] + "px";
+    
+    this.newBlock.style.top = this.posX[random_top_num] + "px";
+    this.newBlock.style.left = this.posY[random_left_num] + "px";
 
     return block_position;
   }
-}
 
-var block = new Block([24, 178, 332, 486]); // Block 객체 생성
-for(var i = 0; i < 2; i++) {
-  var b_pos = block.RandomCreate();
-  empty_arr[b_pos] = 1;
-  console.log(empty_arr);
+    // var px_delete = this.blockArr.style.left.replace('px', '');
+    // console.log(px_delete);
+    //blocks[i].style.left.replace('px', '');
+        // var temporary;
+        
+        // for(j = 1; j < 4; j ++) {
+        //   temporary = px_delete - (154 * j);
+        //   if(temporary < 0) break; // 블록이 범위 밖으로 나가지 않게 방지
+          
+        //   // empty_arr에서의 1 이동 코드 구현
+        //   blocks[i].style.left = temporary + "px";
+
+
+        //   if(blocks[i].style.top == 24 + "px") empty_arr[b] = 1;
+        //   if(blocks[i].style.top == 178 + "px") empty_arr[b] = 1;
+        //   if(blocks[i].style.top == 332 + "px") empty_arr[b] = 1;
+        //   if(blocks[i].style.top == 486 + "px") empty_arr[b] = 1;
+        //}
+  }
+
+var a = new GameManager;
+
+function LeftMove() {
+  var is_stop = new Array();
+  is_stop = false;
+
+  for(i = 0; i < blocks.length; i++) {
+    var px_delete = blocks[i].style.left.replace('px', '');
+    var move_num = parseInt(px_delete) - 10;
+    blocks[i].style.left = move_num + "px";
+
+    if(move_num <= 0) is_stop[i] = true;
+  }
+  return is_stop;
 }
 
 // 블록 이동
-document.addEventListener('keydown', (event) => {
-  for(var i = 0; i < empty_arr.length; i++) empty_arr[i] = 0; // 빈칸 배치 초기화
-
-    if(event.keyCode == 37) { // 왼쪽 키  
-      var blocks = document.getElementsByClassName("fillBox");
-      var b_pos = block.RandomCreate();
-
-      for (var i = 0; i < blocks.length; i++){
-        blocks[i].style.left = "24px"; 
-      }
-    }
-
-    else if(event.keyCode == 38) { // 위쪽 키
-      var blocks = document.getElementsByClassName("fillBox");
-
-      for (var i = 0; i < blocks.length; i++){
-        blocks[i].style.top = "24px";                     
-      }
-      var b_pos = block.RandomCreate();
-      empty_arr[b_pos] = 1;
-    }
-
-    else if(event.keyCode == 39) { // 오른쪽 키
-      var blocks = document.getElementsByClassName("fillBox");
-
-      for (var i = 0; i < blocks.length; i++){
-        blocks[i].style.left = "486px"; 
-
-                           
-      }
-      var b_pos = block.RandomCreate();
-    }
-
-    else if(event.keyCode == 40) { // 아래쪽 키
-      var blocks = document.getElementsByClassName("fillBox");
-
-      for (var i = 0; i < blocks.length; i++){
-        blocks[i].style.top = "486px";                     
-      }
-      var b_pos = block.RandomCreate();
-      empty_arr[b_pos] = 1;
-    }
-    
-  });
+window.addEventListener("keydown", Blockmove, false)
+function Blockmove(e) {
+	switch(e.keyCode) {
+		case 37:
+      var anim = setInterval(LeftMove, 20);
+			break;
+    case 38:
+        // up key pressed
+      break;
+    case 39:
+          // right key pressed
+      break;
+    case 40:
+            // down key pressed
+      break;	
+  }	
+  a.ObjectPool(); 
+  console.log(LeftMove())
+  //if(LeftMove() == true) console.log("sdasd")
+}
 
 function Keylog(e) {
   //console.log(e);
 }
 window.onkeydown = Keylog;
-// window.onkeydown = (e) => {console.log(e);}; // 람다를 사용하면 더 짧게 코드를 줄일 수 있다.
+// window.onkeydown = (e) => {console.log(e);}; // 람다를 사용하면 더 짧게 코드를 줄일 수 있다.  
